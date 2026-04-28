915c6ac981a83e3b38c7f6b08721b8410cdad4c8
DiemLy <nguyenthidiemly2004@gmail.com>
2026-04-28 22:23:29 +0700

    Prepare backend for Cloud Run

--- server/src/db/pool.ts
+++ server/src/db/pool.ts
@@ -1,11 +1,13 @@
 import { Pool } from "pg";
 import { env } from "../config/env";
 
+const isCloudSqlSocket = env.DB_HOST.startsWith("/cloudsql/");
+
 export const pool = new Pool({
   host: env.DB_HOST,
-  port: env.DB_PORT,
+  port: isCloudSqlSocket ? undefined : env.DB_PORT,
   database: env.DB_NAME,
   user: env.DB_USER,
   password: env.DB_PASSWORD,
-  ssl: env.DB_SSL ? { rejectUnauthorized: false } : false,
+  ssl: isCloudSqlSocket ? false : env.DB_SSL ? { rejectUnauthorized: false } : false,
 });
